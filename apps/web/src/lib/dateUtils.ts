ea9af67e7926409795b64abb4e4a124e8bc39176
gustavoparis <gustavo.paris@ixcsoft.com.br>
2026-03-13 10:01:50 -0300

    chore: remove deprecated dateUtils.ts shim

--- apps/web/src/lib/dateUtils.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated Import from '@/lib/date-utils' instead.
- * This file is a re-export shim for backward compatibility.
- */
-export { calculateAge, formatDateBR, toISODateString } from './date-utils';
